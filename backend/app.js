26cc3ab239efaea78611dcc63de544ec0944d17d
Abhiram <abramjoshi@gmail.com>
2022-06-25 13:24:20 +0530

    fixed function attaching user to request

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,8 @@ const cors = require("cors");
 const userRouter = require("./routers/user");
 const topicRouter = require("./routers/topic");
 
+const attachUser = require("./middlewares/attachUser");
+
 dotenv.config();
 
 const app = express();
@@ -16,7 +18,7 @@ app.use(express.json());
 
 // Routes
 app.use("/user", userRouter);
-app.use("/topic", topicRouter);
+app.use("/topic", attachUser, topicRouter);
 
 // Database connection
 mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@code-battle.sju91.mongodb.net/Code-Battle?retryWrites=true&w=majority`).then(() => {
