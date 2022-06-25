556b38bf62304b833dc1f0ba5a9d27fbbec87dee
Abhiram <abramjoshi@gmail.com>
2022-06-25 11:30:04 +0530

    feat: added create user functionality

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const userRouter = require("./routers/user");
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+
+// Routes
+app.use("/user", userRouter);
+
+// Database connection
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@code-battle.sju91.mongodb.net/Code-Battle?retryWrites=true&w=majority`).then(() => {
+    app.listen(3000);
+}).catch(err => {
+    console.log(err);
+})
---
+++ backend/models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+    },
+    uuid: {
+        type: String,
+    },
+    isOAuth: {
+        type: Boolean,
+        required: true,
+    }
+});
+
+module.exports = mongoose.model("user", userSchema);
