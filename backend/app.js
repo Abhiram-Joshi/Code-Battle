f5a95b5bb76d0afeb40dfac6e07cad07792fdba9
shashank-suresh <shashanksuresh3@gmail.com>
2022-06-25 12:39:07 +0530

    Added topic router, model, and controller (getAllTopics function)

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 const userRouter = require("./routers/user");
+const topicRouter = require("./routers/topic");
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 
 // Routes
 app.use("/user", userRouter);
+app.use("/topic", topicRouter);
 
 // Database connection
 mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@code-battle.sju91.mongodb.net/Code-Battle?retryWrites=true&w=majority`).then(() => {
