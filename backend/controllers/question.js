e6772e03af9ebbd86ea4f431522202995d802122
Abhiram <abramjoshi@gmail.com>
2022-06-26 01:57:57 +0530

    added function to return random question

--- backend/controllers/question.js
+++ backend/controllers/question.js
@@ -1,20 +1,22 @@
 const Question = require('../models/question');
+const Topic = require('../models/topic');
 
 exports.getQuestion = (topic, difficulty) => {
     return new Promise((resolve, reject) => {
-        var random;
-        Question.countDocuments({ topic: topic, difficulty: difficulty }, (err, count) => {
-            random = Math.floor(Math.random() * count);
-        });
-
-        // await Question.find({ topic: topic, difficulty: difficulty })
-        Question.find({ topic: topic, difficulty: difficulty })
-            .then(question => {
-                resolve(question[random]);
-            })
-            .catch(err => {
-                console.log(err);
+        Topic.findOne({ name: topic }).then(topic => {
+            const topicID = topic._id;
+            console.log(topicID);
+            var random;
+            Question.countDocuments({ topic: topicID, difficulty: difficulty }).then( count => {
+                random = Math.floor(Math.random() * count);
+                Question.find({ topic: topicID, difficulty: difficulty })
+                .then(question => {
+                    resolve(question[random]);
+                })
+                .catch(err => {
+                    reject(err);
+                });
             });
+        });
     });
-    // return question;
 }
