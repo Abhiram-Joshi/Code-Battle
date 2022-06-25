b2242d6463f260e61fb2f786563ca0b2e85eaeb6
Abhiram <abramjoshi@gmail.com>
2022-06-25 12:08:07 +0530

    added cors policy

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 const userRouter = require("./routers/user");
 
@@ -9,6 +10,7 @@ dotenv.config();
 const app = express();
 
 // Middlewares
+app.use(cors());
 app.use(express.json());
 
 // Routes
