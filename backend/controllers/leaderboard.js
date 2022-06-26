2197b9471c1026810e9ad009c5add332b507a6b9
shashank-suresh <shashanksuresh3@gmail.com>
2022-06-26 12:04:23 +0530

    Removed unnecessary imports and whitespace

--- backend/controllers/leaderboard.js
+++ backend/controllers/leaderboard.js
@@ -1,6 +1,5 @@
 const Leaderboard = require('../models/leaderboard');
 
 exports.createLeaderboardUser = async (email, categoryName) => {
-    
     await Leaderboard.updateOne({ email: email }, { email: email, categoryName: categoryName, points: 0 }, { upsert: true });
 }
