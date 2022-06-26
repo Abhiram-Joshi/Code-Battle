3f9e188690af3150413eefbc93ae11d5f9e078e2
shashank-suresh <shashanksuresh3@gmail.com>
2022-06-26 09:05:40 +0530

    Added points to user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -17,6 +17,11 @@ const userSchema = new Schema({
     isOAuth: {
         type: Boolean,
         required: true,
+    },
+    points: {
+        type: Integer,
+        required: true,
+        default: 0,
     }
 });
 
