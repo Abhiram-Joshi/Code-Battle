67924fd87fe2368abf0af98b2d8d7cc2d3c6a6a3
shashank-suresh <shashanksuresh3@gmail.com>
2022-06-26 12:07:26 +0530

    Added getOverallLeaderboard function

--- backend/controllers/leaderboard.js
+++ backend/controllers/leaderboard.js
@@ -3,3 +3,20 @@ const Leaderboard = require('../models/leaderboard');
 exports.createLeaderboardUser = async (email, categoryName) => {
     await Leaderboard.updateOne({ email: email }, { email: email, categoryName: categoryName, points: 0 }, { upsert: true });
 }
+
+exports.getOverallLeaderboard = async (req, res) => {
+    try {
+        const leaderboard = await Leaderboard.find({}).sort({ points: -1 }).limit(5);
+        res.status(200).json({
+            status: "success",
+            message: "Overall leaderboard",
+            data: leaderboard,
+        });
+    } catch (err) {
+        res.status(500).json({
+            status: "error",
+            message: err.message,
+            data: null,
+        });
+    }
+}
